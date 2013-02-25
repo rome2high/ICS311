72069f3107b1174502fb6877b66a0e349b775cbc
rome2high <mnsaigonese@gmail.com>
2013-02-25 13:08:28 -0800

    New java/com/abc/jdbc/update/Update.java

--- HW04_JDBC_Update/src/java/com/abc/jdbc/update/Update.java
+++ HW04_JDBC_Update/src/java/com/abc/jdbc/update/Update.java
@@ -8,24 +8,40 @@ public class Update {
      */
     public static void main(String[] args) {
         
-      System.out.println("damn blue angle");
       String driver = "org.hsqldb.jdbcDriver";
       String url = "jdbc:hsqldb:hsql://localhost:3500";
       String user = "sa";
       String pass = "";
       
+      Connection con = null;
+      Statement deleteStmt = null;
+      PreparedStatement insertStmt = null;
+      String sql  = "";
+      
       try {
           Class.forName(driver);
-          System.out.println("load the driver!");
-          Connection con = DriverManager.getConnection(url, user, pass);
-          Statement stmt = con.createStatement();
-          int result = stmt.executeUpdate(
-              "INSERT INTO customer " +
-              "(id, lname, fname) " +
-              "VALUES ('2323', 'jr' , 'Mai')"
-          );
-          System.out.println(result);
-          stmt.close();
+          con = DriverManager.getConnection(url, user, pass);
+          
+          sql = "UPDATE customer SET EMAIL = ? WHERE ID  = ? ";
+          
+          insertStmt = con.prepareStatement(sql);
+          insertStmt.setString(1,"Jane.Johnson@abc.com");
+          insertStmt.setInt(2,2001);
+          
+          System.out.println("About to execute: " + sql);
+          int resultCode = insertStmt.executeUpdate();
+          System.out.println("Result code: " + resultCode);
+
+
+//          int result = stmt.executeUpdate(
+//              "INSERT INTO customer " +
+//              "(id, lname, fname) " +
+//              "VALUES ('2323', 'Romeo' , 'Mai')"
+//          );
+//          System.out.println(result);
+
+          insertStmt.close();
+          deleteStmt.close();
           con.close();
       } catch ( ClassNotFoundException x) {
           x.printStackTrace();
